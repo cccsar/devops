14d2e0923167a502b0c362c944dee27b1868bbf2
cccsar <15-11295@usb.ve>
2019-11-03 10:23:12 -0400

    mas

--- test.c
+++ test.c
@@ -13,14 +13,8 @@
 #define MY_PERM S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH
 #define PERM_UMASK S_IRWXU & S_IRWXG & S_IRWXO
 
+
+
 int main (int argc, char **argv) { 
-
-	char prueba, otra, otra_mas;
-	prueba = 'a'; 
-	scanf("%c",&otra_mas); 
-	printf("%d\n",prueba);
-	printf("%d\n",otra_mas);
-
-	return 0; 
+	return 0;
 }
-
