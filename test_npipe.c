3c9289b9b4e4baa4f3ea4d2ed9dbdf6bf6f1bf64
cccsar <15-11295@usb.ve>
2019-11-26 05:13:56 -0400

    mas

--- test_npipe.c
+++ test_npipe.c
@@ -11,17 +11,26 @@
 
 #define TRUE 1
 #define FALSE 0
+
 #define READ 0
 #define WRITE 1
 
 int main (int argc, char **argv) { 
 
-	int fd[2], st_1, st_2; 
+	int fd, st_1, st_2; 
 	pid_t ps_1, ps_2; 
 	
+	if( unlink("npipe") == -1)
+		perror("unlink");
+	if( mknod("npipe",S_IFIFO,0) == -1)
+		perror("mknod");
+	if( chmod("npipe",0666) == -1)
+		perror("chmod");
 
-	if( pipe(fd) == -1)
-		perror("pipe"); 
+	if( (fd = open("npipe", O_RDWR | O_APPEND) ) == -1)
+		perror("open");
+	printf("fd: %d\n",fd);
+
 
 	/* First process: writer */ 
 	ps_1 = fork(); 
@@ -30,16 +39,13 @@ int main (int argc, char **argv) {
 
 	if (ps_1 == 0) { 
 
-		close(fd[READ]); 
+		printf("llego pid %d ejecutando %s\n",getpid(), argv[1]);
 
-		printf("llego pid %d\n",getpid());
-		printf("ejecutando %s\n",argv[1]);
-		dup2(fd[WRITE],1); 
+		dup2(fd,1); 
+		close(fd);
 
-		close(fd[WRITE]); 
-
-		execlp(argv[1], argv[1], (char *) NULL) ;
-		perror("execlp"); 
+		execlp(argv[1], argv[1], "." , (char *) NULL) ; 
+		perror(argv[1]);
 		exit(0);  
 	}
 
@@ -49,31 +55,29 @@ int main (int argc, char **argv) {
 		perror("fork");
 
 	if (ps_2 == 0) {
-		close(fd[WRITE]); 
+		dup2(fd, 0); 
+		close(fd);
 
-		dup2(fd[READ], 0); 
+		printf("llego pid %d ejecutando %s\n",getpid() ,argv[2]);
 
-		close(fd[READ]); 
-
-		printf("llego pid %d\n",getpid());
-		printf("ejecutando %s\n",argv[2]);
 		execlp(argv[2], argv[2], "-l", (char*) NULL)  ;
-		perror("execlp");
+		perror(argv[2]);
 		exit(0); 
 	}
 
 
-	/*printf("el padre %d llego pid \n",getpid());*/
-	close(fd[READ]); 
-	close(fd[WRITE]); 
+	close(fd);
+	printf("el padre llego pid %d \n",getpid());
 	if ( wait(&st_1) == -1) 
 		perror("wait");
-	/*printf("termino %d\n",ps_1);*/
+	printf("termino %d\n",ps_1);
 
 	if ( wait(&st_2) == -1) 
 		perror("wait");
-	/*printf("termino %d\n",ps_2);*/
+	printf("termino %d\n",ps_2);
 
+
+	sleep(10);
 
 	return 0; 
 }
