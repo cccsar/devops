bb7f1d486664395d33eca6f66511620e3696ce3c
cccsar <15-11295@usb.ve>
2020-02-22 21:07:34 -0400

    added backtracking for permutations

--- minip/graph/backtracing/test.cpp
+++ minip/graph/backtracing/test.cpp
@@ -25,12 +25,56 @@ typedef pair <int, int> pi;
 typedef pair <int, pair<int, int> > pii; 
 typedef vector< vi > adl; 
 typedef vector< vii > wadl; 
+
+typedef vector<char> word; 
 	
+void foo(word hola, word mas[], char n) { 
+	hola.push_back(n+97); 
+
+	for(char k : hola)
+		cout<<k<<" ";
+	cout<<endl;
+
+	if ( n != 0 )  
+		foo(hola, mas, n-1); 
+	else 
+		mas[0] = hola; 
+}
+
 
 int main() 
 {
-	char a; 
-	scanf("%c",&a); 
-	cout<<a<<endl;
-	return 0; 
+	int i_, n, size; 
+	char e; 
+	deque<char> test; 
+
+	ri(n); 
+	for(i_=0; i_<n ; i_++) {
+		scanf(" %c",&e); 	
+		test.push_back(e); 
+	}	
+
+	size = test.size(); 
+	char temp; 
+
+	cout<<"first test: "<<endl;
+
+	for(char k : test) 
+		cout<<k<<" ";
+	cout<<endl; 
+
+	for(i_=0; i_<size ; i_++) {
+		temp = test.back(); 		
+		test.pop_back(); 
+
+		cout<<"current: "<<temp<<endl; 
+		cout<<"complement: "<<endl;
+		for(char k : test)
+			cout<<k<<" "; 
+		cout<<endl ;
+
+		test.push_fron(temp);
+	}
+
+	return 0 ;
 }
