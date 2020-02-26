70467a54179574f4ea46dbb7dc7b8e9718e03a42
Shinji Ikari <shinji@tato-00.tokio-03>
2020-02-26 17:21:15 -0400

    update del script

--- script.sh
+++ script.sh
@@ -14,14 +14,20 @@ RESULT=`ping -q -c 4 -W 5 $1 | tee log_fallas.txt`;
 
 
 #verfica que haya perdida de paquetes
-if [ `echo $RESULT | grep "packet loss" | cut -d "," -f 3 | cut -d "%" -f 1 "` -lt 100 ] ; then 
-	echo "Package loss.\nSending mail to administrator" ; 
+if [ `echo $RESULT | grep "packet loss" | cut -d "," -f 3 | cut -d "%" -f 1 ` -lt 100 ] ; then 
+	echo -e "Package loss.\nSending mail to administrator..." ; 
 
 	#obtiene el correo del administrador del servidor DNS del dominio consultado
-	FULL=`host -t SOA $1 | cut -d " " -f 1 | cut -d "." -f 1,3`;
-	UNAME=`echo $FULL | cut -d "." -f 2-`; 
-	DOMAIN=`echo $FULL | cut -d `;
+	FULL=`host -t SOA $1 | cut -d " " -f 6 `;
 
+	UNAME=`echo $FULL | cut -d "." -f 1`;
+	DOMAIN=`echo $FULL | cut -d "." -f 2-`; 
+
+	echo "$UNAME@$DOMAIN";
+	echo 	"To: $UNAME@$DOMAIN\n
+		From: \n 
+		Subject: Dominio caido \n
+		Buenas, el dominio $1 esta caido" | sendmail -vt;
 	exit 2 ; 
 fi
 
