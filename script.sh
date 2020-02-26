b598b4dfa4adfc625644c8fd2e1254461c92a3ad
Shinji Ikari <shinji@tato-00.tokio-03>
2020-02-26 19:41:26 -0400

    mas

--- script.sh
+++ script.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-DNS_REGEX="([[:alpha:]]+\.)+[[:alpha:]]+";
-
-TEST=`echo $1 | grep -E '$DNS_REGEX'`;
+TEST=`echo $1 | grep -E '([[:alpha:]]+\.)+[[:alpha:]]+'`;
 
 #Verifica que el nombre de dominio escrito sea de hecho un nombre de dominio
 if [ $TEST != $1 ]; then
@@ -15,13 +13,13 @@ fi
 while (true) ; do 
 
 	#hace ping al dominio solicitado, envia los resultados a un log y los almacena en una variable
-	RESULT=`ping -q -c 4 -W 5 $1 | tee log_fallas.txt`;
+	RESULT=`ping -q -c 5 -W 5 $1 | tee log_fallas.txt`;
 	
 	PACKET_LOSS=`echo $RESULT | grep "packet loss" | cut -d "," -f 3 | cut -d "%" -f 1 `;
 
-
+	echo $PACKET_LOSS;
 	#verfica que haya perdida de paquetes
-	if [ $PACKET_LOSS -eq 0 ] ; then 
+	if [ $PACKET_LOSS -gt 0 ] ; then 
 		echo -e "Perdida de paquetes.\n Enviando correo a administrador de dominio."; 
 	
 		#obtiene el correo del administrador del servidor DNS del dominio consultado
@@ -30,7 +28,6 @@ while (true) ; do
 		UNAME=`echo $FULL | cut -d "." -f 1`;
 		DOMAIN=`echo $FULL | cut -d "." -f 2-`; 
 	
-		echo "$UNAME@$DOMAIN";
 		echo -e	"To: $UNAME@$DOMAIN\n
 			From: $USER@$HOSTNAME\n 
 			Subject: Dominio caido \n
