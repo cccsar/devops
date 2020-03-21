d94ff99e9454000df63989de3b49c30c1e6430d0
Shinji Ikari <shinji@tato-00.tokio-03>
2020-03-21 16:19:41 -0400

    update?

--- mini-proyects/my_coreutils/working/test.c
+++ mini-proyects/my_coreutils/working/test.c
@@ -9,6 +9,7 @@
 #include <string.h> 
 #include <unistd.h>
 #include <string.h>
+#include <signal.h> 
 
 #define TRUE 1
 #define FALSE 0
@@ -18,13 +19,43 @@
 #define TOK 'a'
 #define REP '$'
 
+int guard; 
 
-int main(void)
+void intHandler() { 
+	guard = FALSE; 
+	printf("guard: %d\taddress:%p\n",guard, &guard); 
+}
+
+int main(int argc, char *argv[])
 {
-  int i, j, n;
+  int  i_, k;
+  char hola[BUFFS]; 
 
-  printf("\033[1;31m"); 
-  printf("hola\n"); 
+  void (*old_sh)();
+  __sighandler_t old_h; 
+
+  guard = TRUE; //init breaker
+
+  old_sh = signal(SIGINT, intHandler ) ; //update handler
+
  
+  while ( guard ) { //think on how to interrupt any time 
+
+	k = read(0, hola, BUFFS); 
+
+	printf("ARRIBA\n"); 
+	for(i_=0; i_<k ;i_++) { 
+		if (hola[i_] == 'a' ) 
+			printf("found\n"); 
+	}	
+
+	if ( !guard) 
+		break; 
+	printf("guard: %d\n",guard); 
+	printf("ABAJO\n");
+  }
+
+  signal(SIGINT , old_sh); 
+  
   return 0; 
 }
